c195e6c85e0b7e9e9a253674bbdcee6ba5786af7
Nocky <cocytus1223@gmail.com>
2019-01-23 21:50:21 +0800

    order页面+登录授权

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,5 +1,6 @@
 import wepy from 'wepy'
-const BASE_URL = 'https://itjustfun.cn/api/public/v1/'
+const BASE_URL_REMOTE = 'https://itjustfun.cn/api/public/v1/'
+const BASE_URL_LOCALE = 'http://localhost:8888/api/public/v1/'
 export default function fetch(url, options = {}) {
   if (typeof url === 'string') {
     options.url = url
@@ -7,8 +8,14 @@ export default function fetch(url, options = {}) {
   if (typeof url === 'object') {
     options = url
   }
+
+  if (options.type === 'local') {
+    options.url = BASE_URL_LOCALE + options.url
+  } else {
+    options.url = BASE_URL_REMOTE + options.url
+  }
   return wepy.request({
-    url: BASE_URL + options.url,
+    url: options.url,
     method: options.method || 'GET',
     data: options.data || {}
   })
